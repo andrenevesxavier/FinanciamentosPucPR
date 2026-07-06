a55d69eb53bb197b63ee738aadcc5137175c550a
andrenevesxavier <andrenevesxavier276@gmail.com>
2026-07-06 20:02:54 -0300

    refactor: cria classe de exceção

---
+++ src/Exeptions/AumentoMaiorDoQueJurosException.java
@@ -0,0 +1,8 @@
+package Exeptions;
+
+public class AumentoMaiorDoQueJurosException extends RuntimeException {
+
+    public  AumentoMaiorDoQueJurosException(String mensagem) {
+        super(mensagem);
+    }
+}
