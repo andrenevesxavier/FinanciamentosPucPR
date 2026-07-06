17896cb5db2fab58ed9cc27974ae33bb6a1bf365
andrenevesxavier <andrenevesxavier276@gmail.com>
2026-07-05 21:18:19 -0300

    feat: Adicionar metodos para usuario poder interagir com os novos campos criados.

--- src/util/InterfaceUsuario.java
+++ src/util/InterfaceUsuario.java
@@ -88,5 +88,133 @@ public class InterfaceUsuario {
         }
         return taxa;
     }
+
+    public static int medidaTerreno() {
+        int tamanhoTerreno = 0;
+        boolean controle = false;
+
+        while(!controle) {
+            try {
+                System.out.println("\nDigite o tamanho do terreno (em m²");
+                tamanhoTerreno = scanner.nextInt();
+
+                if (tamanhoTerreno > 1) {
+                    System.out.println("Tamannho do terreno adicionado com sucesso\n");
+                    controle = true;
+
+                } else {
+                    System.out.println("\nOcorreu um erro, digite novamente\n");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("\nEntrada invalida, digite um número");
+                scanner.nextLine();
+            }
+        }
+
+        return tamanhoTerreno;
+    }
+
+    public static String tipoZona() {
+
+        String tipoLocal = null;
+        boolean controle = false;
+
+        while(!controle) {
+
+            try {
+                System.out.println("\nDigite o tipo de area onde o terreno fica localizado (residencial ou comercial)");
+                tipoLocal = scanner.nextLine();
+                controle = true;
+
+            } catch (InputMismatchException e) {
+                System.out.println("\nEntrada invalida, Tente novamente");
+                scanner.nextLine();
+            }
+        }
+        return tipoLocal;
+    }
+
+    public static int areaConstruida() {
+
+        boolean controle = false;
+        int areaDeConstrucao = 0;
+
+
+        while(!controle) {
+            try {
+                System.out.println("\nDigite o tamanho da área contruída da casa (em m²");
+                areaDeConstrucao = scanner.nextInt();
+
+                if (areaDeConstrucao > 1) {
+                    System.out.println("Tamannho do terreno adicionado com sucesso\n");
+                    controle = true;
+
+                } else {
+                    System.out.println("\nOcorreu um erro, digite novamente\n");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("\nEntrada invalida, digite um número");
+                scanner.nextLine();
+            }
+        }
+
+        return areaDeConstrucao;
+    }
+
+    public static int vagasGaragem() {
+
+        int vagasGaragem = 0;
+        boolean controle = false;
+
+        while(!controle) {
+            try {
+                System.out.println("Há quantas vagas de garagem diponíveis do apartamento");
+                vagasGaragem = scanner.nextInt();
+                scanner.nextLine();
+
+                if (vagasGaragem > 1) {
+                    System.out.println("Ok, apartamento com " + vagasGaragem + " vagas de garagem");
+                    controle = true;
+                } else {
+                    System.out.println("\nOcorreu um erro, digite novamente\n");
+                }
+
+            }catch (InputMismatchException e){
+                System.out.println("\nEntrada invalida, digite um número");
+                scanner.nextLine();
+            }
+        }
+
+        return vagasGaragem;
+    }
+
+    public static int andarApartamento() {
+        int andarApartamento = 0;
+        boolean controle = false;
+
+
+        while(!controle) {
+            try {
+                System.out.println("Há quantas vagas de garagem diponíveis do apartamento");
+                andarApartamento = scanner.nextInt();
+                scanner.nextLine();
+
+                if (andarApartamento > 0) {
+                    System.out.println("Ok, apartamento localizado no " + andarApartamento + " andar");
+                    controle = true;
+                } else {
+                    System.out.println("\nOcorreu um erro, digite novamente\n");
+                }
+
+            }catch (InputMismatchException e){
+                System.out.println("\nEntrada invalida, digite um número");
+                scanner.nextLine();
+            }
+        }
+        return andarApartamento;
+    }
+
 }
 
