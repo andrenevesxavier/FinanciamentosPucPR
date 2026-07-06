c5075a21f4559cf709dfa9c1479bd84b7e78c29c
andrenevesxavier <andrenevesxavier276@gmail.com>
2026-07-05 21:20:27 -0300

    refactor: Adaptar a main para os novos atributos criados nas classes.

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,31 +8,71 @@ import util.InterfaceUsuario;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         List<Financiamento> financiamentos = new ArrayList<Financiamento>();
 
         for (int i = 0; i < 1; i++) {
 
-            System.out.println("\nDigite os dados do " + (i + 1) + "º financiamento:");
+            System.out.println("Qual tipo de financiamento você deseja?\n" +
+                    "1 - Casa\n" +
+                    "2 - Apartamento\n" +
+                    "3 - Terreno\n");
+            var entradaUsuario = sc.nextInt();
+            sc.nextLine();
 
-            double valorDoImovel = InterfaceUsuario.valorImovel();
-            int prazo = InterfaceUsuario.prazoFinanciamento();
-            double taxa = InterfaceUsuario.taxaJuros();
+            if (entradaUsuario == 1) {
+                System.out.println("\nDigite os dados do " + (i + 1) + "º financiamento do terreno:");
 
-            Financiamento financiamento = new Terreno(valorDoImovel, prazo, taxa);
-            financiamento.exibirMensagem();
-            financiamentos.add(financiamento);
+                double valorDoImovel = InterfaceUsuario.valorImovel();
+                int prazo = InterfaceUsuario.prazoFinanciamento();
+                double taxa = InterfaceUsuario.taxaJuros();
+                int areaConstruida = InterfaceUsuario.areaConstruida();
+                int tamanhoTerreno = InterfaceUsuario.medidaTerreno();
+
+                Financiamento financiamento = new Casa(valorDoImovel, prazo, taxa, areaConstruida, tamanhoTerreno);
+                financiamento.exibirMensagem();
+                financiamentos.add(financiamento);
+            }
+
+            if (entradaUsuario == 2) {
+                double valorDoImovel = InterfaceUsuario.valorImovel();
+                int prazo = InterfaceUsuario.prazoFinanciamento();
+                double taxa = InterfaceUsuario.taxaJuros();
+                int vagaGaragem = InterfaceUsuario.vagasGaragem();
+                int numeroAndar = InterfaceUsuario.andarApartamento();
+
+                Financiamento financiamento = new Apartamento(valorDoImovel, prazo, taxa, vagaGaragem, numeroAndar);
+
+                financiamento.exibirMensagem();
+                financiamentos.add(financiamento);
+
+            }
+
+            if (entradaUsuario == 3) {
+                System.out.println("\nDigite os dados do " + (i + 1) + "º financiamento do terreno:");
+
+                double valorDoImovel = InterfaceUsuario.valorImovel();
+                int prazo = InterfaceUsuario.prazoFinanciamento();
+                double taxa = InterfaceUsuario.taxaJuros();
+                String tipoTerreno = InterfaceUsuario.tipoZona();
+
+                Financiamento financiamento = new Terreno(valorDoImovel, prazo, taxa, tipoTerreno);
+                financiamento.exibirMensagem();
+                financiamentos.add(financiamento);
+            }
         }
 
-        financiamentos.add(new Casa(150000, 20, 10));
-        financiamentos.add(new Casa(180000, 15, 12));
-        financiamentos.add(new Apartamento(250000, 25, 9));
-        financiamentos.add(new Apartamento(320000, 30, 11));
+        financiamentos.add(new Casa(150000, 20, 10, 50, 80));
+        financiamentos.add(new Casa(180000, 15, 12, 100, 80));
+        financiamentos.add(new Apartamento(250000, 25, 9, 2, 6));
+        financiamentos.add(new Apartamento(320000, 30, 11, 1, 3));
 
 
         int i = 1;
